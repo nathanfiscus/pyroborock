043f6bc98885b98003cf902083d636788fd4e79d
Nathan Fiscus <nathanfiscus@users.noreply.github.com>
2021-12-15 22:19:34 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ def read(fname):
 
 setup(
     name='pyroborock',
-    version='1.1.2',
+    version='1.1.3',
     packages=['pyroborock'],
-    install_requires=['tinytuya==v1.2.3', 'python-miio==0.5.6', 'pycryptodome==3.9.8'],
+    install_requires=['tinytuya==v1.2.11', 'python-miio==0.5.9.2', 'pycryptodome==3.9.8'],
     description='Communicate with roborock over tuya protocol',
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
-    url='https://github.com/89jd/pyroborock',
-    author='jd89',
-    author_email='jd89.dev@gmail.com',
+    url='https://github.com/nathanfiscus/pyroborock',
+    author='nathanfiscus',
+    author_email='nathan_fiscus@hotmail.com',
 )
